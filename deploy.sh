0a7e95aa48a0ffa6422530ba99da49bbd7787a8b
Juan Manuel Carrillo Moreno <inetshell@gmail.com>
2018-10-14 02:38:44 -0500

    Remove unused lines from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 export SCRIPT_HOME="$( cd "$(dirname "${0}")" ; pwd -P )"
 
-mkdir -p ${SCRIPT_HOME}/volumes/rancher-mysql
-mkdir -p ${SCRIPT_HOME}/volumes/rancher-mysql-log
-mkdir -p ${SCRIPT_HOME}/volumes/rancher-cattle
-mkdir -p ${SCRIPT_HOME}/volumes/frontend-log
-
 if [ -z "${CERTBOT_EMAIL}" ] || [ -z ${RANCHER_DNS} ]; then
     echo "CERTBOT_EMAIL or RANCHER_DNS variables are not defined"
     exit 1
