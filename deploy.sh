92d3f6971f5b8266fd1e617d585c8822a79d0454
Juan Manuel Carrillo Moreno <inetshell@gmail.com>
2018-10-14 01:52:58 -0500

    Fix error in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ mkdir -p ${SCRIPT_HOME}/volumes/rancher-mysql-log
 mkdir -p ${SCRIPT_HOME}/volumes/rancher-cattle
 mkdir -p ${SCRIPT_HOME}/volumes/frontend-log
 
-if [[ -z ${CERTBOT_EMAIL} ] || [ -z ${RANCHER_DNS} ]]; then
+if [ -z "${CERTBOT_EMAIL}" ] || [ -z ${RANCHER_DNS} ]; then
     echo "CERTBOT_EMAIL or RANCHER_DNS variables are not defined"
     exit 1
 fi
